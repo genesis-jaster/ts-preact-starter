baea6b550e88efb940db9dd0352332eb954500cb
Nick Taylor <nick@iamdeveloper.com>
2017-11-20 07:23:43 -0500

    Added types for Storybook.

--- workbench/todo-item.tsx
+++ workbench/todo-item.tsx
@@ -2,8 +2,6 @@ import { storiesOf } from '@kadira/storybook';
 import { h } from 'preact';
 import TodoItem from '../src/components/todo-item';
 
-declare var module;
-
 storiesOf('Todo Item', module).add('with text', () => {
   return <TodoItem text="pick up milk" />;
 });
